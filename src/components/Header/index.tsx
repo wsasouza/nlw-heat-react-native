cf1d6dff2bec9895a0be60a745cf86a77588f5f7
wsasouza <wsasouza@hotmail.com>
2021-10-21 21:59:52 -0300

    Feat: Header Avatar Image

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { UserPhoto } from '../UserPhoto';
+
+import { styles } from './styles';
+import LogoSvg from '../../assets/logo.svg';
+
+export function Header(){
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <UserPhoto imageUri='https://github.com/wsasouza.png'/>
+
+      <TouchableOpacity>
+        <Text style={styles.logoutText}>Sair</Text>    
+      </TouchableOpacity>  
+    </View>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { Header } from '../../components/Header';
+
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Olá React Native</Text>
     </View>
   );
