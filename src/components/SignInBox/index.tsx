57d744437225e8caa8c63e3aa8a81a405999ad23
wsasouza <wsasouza@hotmail.com>
2021-10-23 17:45:16 -0300

    Feat: Button and Sign In Box ok

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Button } from '../Button';
+
+import { styles } from './styles';
+import { COLORS } from '../../theme';
+
+export function SignInBox(){
+  return (
+    <View style={styles.container}>
+      <Button 
+        title="ENTRAR COM O GITHUB" 
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"        
+      />
+    </View>
+  );
+}
